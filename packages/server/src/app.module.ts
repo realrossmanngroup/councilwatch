3bea74ca532d6ebe460e16543fbf37464e7d5c6b
scriptis <me@scriptis.net>
2025-10-16 20:52:29 -0500

    fix: use `url` for Postgres URL resolution

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,10 @@
-import { join } from 'node:path';
-import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppConfigModule } from './app-config/app-config.module';
-import { AppConfigService } from './app-config/app-config.service';
-import { UsersModule } from './users/users.module';
+import { join } from "node:path";
+import { Module } from "@nestjs/common";
+import { ScheduleModule } from "@nestjs/schedule";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppConfigModule } from "./app-config/app-config.module";
+import { AppConfigService } from "./app-config/app-config.service";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
@@ -13,17 +13,13 @@ import { UsersModule } from './users/users.module';
       imports: [AppConfigModule],
       inject: [AppConfigService],
       useFactory: (configService) => ({
-        type: 'postgres',
-        applicationName: 'CouncilWatch',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USERNAME'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
+        type: "postgres",
+        applicationName: "CouncilWatch",
+        url: configService.get("DATABASE_URL"),
         synchronize: false,
         autoLoadEntities: true,
         entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-        migrations: [join(__dirname, 'common', 'migrations', '*.{ts,js}')],
+        migrations: [join(__dirname, "common", "migrations", "*.{ts,js}")],
         migrationsRun: true,
       }),
     }),
