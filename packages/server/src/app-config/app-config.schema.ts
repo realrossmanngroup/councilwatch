c0a76e41d2046705ecf09171c0669ef7e0d15ed4
scriptis <me@scriptis.net>
2025-10-16 20:52:29 -0500

    fix: use `url` for Postgres URL resolution

--- packages/server/src/app-config/app-config.schema.ts
+++ packages/server/src/app-config/app-config.schema.ts
@@ -1,25 +1,17 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 export type CouncilWatchConfig = {
   APP_HOST: string;
   APP_PORT: number;
   API_PREFIX: string;
 
-  DATABASE_HOST: string;
-  DATABASE_PORT: number;
-  DATABASE_USERNAME: string;
-  DATABASE_PASSWORD: string;
-  DATABASE_NAME: string;
+  DATABASE_URL: string;
 };
 
 export const appConfigSchema = Joi.object<CouncilWatchConfig, true>({
-  APP_HOST: Joi.string().default('localhost'),
+  APP_HOST: Joi.string().default("localhost"),
   APP_PORT: Joi.number().default(8080),
-  API_PREFIX: Joi.string().default('api'),
+  API_PREFIX: Joi.string().default("api"),
 
-  DATABASE_HOST: Joi.string().required(),
-  DATABASE_PORT: Joi.number().default(5432),
-  DATABASE_USERNAME: Joi.string().required(),
-  DATABASE_PASSWORD: Joi.string().required(),
-  DATABASE_NAME: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
 });
