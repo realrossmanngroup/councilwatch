4c94684076316aeeadd92b272a211870d294ecc4
scriptis <me@scriptis.net>
2025-10-16 22:30:36 -0500

    chore: fmt

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,10 @@
-import { join } from "node:path";
-import { Module } from "@nestjs/common";
-import { ScheduleModule } from "@nestjs/schedule";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppConfigModule } from "./app-config/app-config.module";
-import { AppConfigService } from "./app-config/app-config.service";
-import { UsersModule } from "./users/users.module";
+import { join } from 'node:path';
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from './app-config/app-config.module';
+import { AppConfigService } from './app-config/app-config.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,13 +13,13 @@ import { UsersModule } from "./users/users.module";
       imports: [AppConfigModule],
       inject: [AppConfigService],
       useFactory: (configService) => ({
-        type: "postgres",
-        applicationName: "CouncilWatch",
-        url: configService.get("DATABASE_URL"),
+        type: 'postgres',
+        applicationName: 'CouncilWatch',
+        url: configService.get('DATABASE_URL'),
         synchronize: false,
         autoLoadEntities: true,
         entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-        migrations: [join(__dirname, "common", "migrations", "*.{ts,js}")],
+        migrations: [join(__dirname, 'common', 'migrations', '*.{ts,js}')],
         migrationsRun: true,
       }),
     }),
